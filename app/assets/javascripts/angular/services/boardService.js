6f476d7c9d6ff7514789902cc39e82fed30022e0
James Harris <computersherpa@gmail.com>
2017-02-06 12:07:49 -0600

    indentation

--- app/assets/javascripts/angular/services/boardService.js
+++ app/assets/javascripts/angular/services/boardService.js
@@ -1,50 +1,45 @@
 Chunky.factory('boardService',
-
   ['Restangular',
+    function(Restangular) {
+      var _boards;
+      var exports = {};
 
-  function(Restangular) {
+      exports.all = function() {
+        return Restangular.all('boards').getList()
+          .then(function(data) {
+            return _boards = data;
+          });
+      }
 
-    var _boards;
+      exports.create = function(boardName) {
+        return Restangular.all('boards').post({
+          board: {
+            name: boardName
+          }
+        }).then(function(data) {
+            return data;
+          })
+      }
 
-    var exports = {};
+      exports.show = function(boardId) {
+        return Restangular.one('boards', boardId).get()
+          .then(function(data) {
+            return data;
+          })
+      }
 
-    exports.all = function() {
-      return Restangular.all('boards').getList()
+      exports.delete = function(boardId) {
+        return Restangular.one('boards', boardId).remove()
         .then(function(data) {
-          return _boards = data;
-        });
-    }
-
-    exports.create = function(boardName) {
-      return Restangular.all('boards').post({
-        board: {
-          name: boardName
-        }
-      }).then(function(data) {
           return data;
         })
+      }
+
+      exports.getBoards = function() {
+        return _boards;
+      }
+
+      return exports;
     }
-
-    exports.show = function(boardId) {
-      return Restangular.one('boards', boardId).get()
-        .then(function(data) {
-          return data;
-        })
-    }
-
-    exports.delete = function(boardId) {
-      return Restangular.one('boards', boardId).remove()
-      .then(function(data) {
-        return data;
-      })
-    }
-
-    exports.getBoards = function() {
-      return _boards; 
-    }
-
-    return exports;
-
-  }
-
-])
+  ]
+)
