519ede2236b3518de8b95d4764485b5c9f863461
James Harris <computersherpa@gmail.com>
2017-02-06 16:17:20 -0600

    Seed descriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
+puts "Destroying universe..."
+
+User.destroy_all
+Board.destroy_all
+List.destroy_all
+Card.destroy_all
+
 NUM_USERS = 5
 NUM_BOARDS = 3
 NUM_LISTS = 3
 NUM_CARDS = 5
 
-puts "Seeding..."
+puts "Seeding universe..."
 NUM_USERS.times do
   user = User.create(email: Faker::Internet.email, password: "123456789")
   NUM_BOARDS.times do
@@ -11,9 +18,11 @@ NUM_USERS.times do
     NUM_LISTS.times do
       list = board.lists.create(name: Faker::Space.constellation)
       NUM_CARDS.times do
-        list.cards.create(title: Faker::Space.star)
+        list.cards.create(title: Faker::Space.star, description: Faker::StarWars.wookie_sentence)
       end
     end
   end
 end
 puts "Seeding complete."
+puts "First user:"
+puts User.first.email
