c844a2a990f1e9d96c0cfb1e2a4266024676764e
James Harris <computersherpa@gmail.com>
2017-02-07 16:01:05 -0600

    Model for nested cards

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,5 @@
 class Card < ApplicationRecord
-  belongs_to :list
+  belongs_to :list, optional: true
+  has_many :children, foreign_key: :parent_id, class_name: "Card"
+  belongs_to :parent, optional: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207204515) do
+ActiveRecord::Schema.define(version: 20170207214540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170207204515) do
     t.text     "description"
     t.integer  "order"
     t.boolean  "completed",   default: false
+    t.integer  "parent_id"
   end
 
   create_table "lists", force: :cascade do |t|
