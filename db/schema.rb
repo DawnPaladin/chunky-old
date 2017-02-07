09659f3e4aa3943df8e235077d2a68cadd56ac1f
James Harris <computersherpa@gmail.com>
2017-02-07 15:02:53 -0600

    Completed checkbox

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207171707) do
+ActiveRecord::Schema.define(version: 20170207204515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,11 +24,12 @@ ActiveRecord::Schema.define(version: 20170207171707) do
 
   create_table "cards", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "list_id"
     t.text     "description"
     t.integer  "order"
+    t.boolean  "completed",   default: false
   end
 
   create_table "lists", force: :cascade do |t|
