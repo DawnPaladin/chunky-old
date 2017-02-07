ae03664930aecacfd6e27cf9c30ad4bc872d5404
James Harris <computersherpa@gmail.com>
2017-02-07 10:05:01 -0600

    Add card field

--- app/assets/javascripts/angular/services/listService.js
+++ app/assets/javascripts/angular/services/listService.js
@@ -4,9 +4,17 @@ Chunky.factory('listService',
       var _lists;
       var exports = {};
 
+      var newCard = function() {
+        this.showNewCard = true;
+      };
+
       exports.setup = function(lists) {
         _lists = lists;
         cardService.setup(lists);
+        _lists.forEach(function(list) {
+          list.showNewCard = false;
+          list.newCard = newCard;
+        });
       };
 
       return exports;
