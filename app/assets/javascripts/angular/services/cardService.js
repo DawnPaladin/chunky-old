fdefa6313380683dfdcf7b76448069a3c5131afb
James Harris <computersherpa@gmail.com>
2017-02-07 12:59:21 -0600

    Sort by date updated

--- app/assets/javascripts/angular/services/cardService.js
+++ app/assets/javascripts/angular/services/cardService.js
@@ -15,6 +15,7 @@ Chunky.factory('cardService',
 
       exports.save = function(card) {
         card.put();
+        card.updated_at = new Date;
         // Restangular.one('cards', card.id).put({card: card});
       };
 
